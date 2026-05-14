5d4def2aec3c149f9aa5b4f42ffa8d9a8e4e46c4
Tharun2331 <tharunsd23@gmail.com>
2026-05-13 21:03:25 -0400

    Removed Cron,Modified HashUtils and compose files

--- secondBrain/src/utils/hashUtils.ts
+++ secondBrain/src/utils/hashUtils.ts
@@ -1,13 +1,16 @@
 import dotenv from "dotenv";
 dotenv.config();
-const options = process.env.RANDOM_STRING;
-  
-export function Random(len:number) {
-    const length = (options as string).length;
-    let result="";
-    for(let i=0; i<len; i++)
-    {
-      result+= (options as string)[Math.floor((Math.random()*length))]
-    }
-    return result;
+
+export function Random(len: number) {
+  const options = process.env.RANDOM_STRING;
+  if (!options) {
+    console.warn("RANDOM_STRING is not defined in environment variables. Returning empty string.");
+    return "";
+  }
+  const length = options.length;
+  let result = "";
+  for (let i = 0; i < len; i++) {
+    result += options[Math.floor(Math.random() * length)];
+  }
+  return result;
 }

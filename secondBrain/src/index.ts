ad6f507542542d70ea92127a66d5108f23ab5c3e
Tharun2331 <tharun@LAPTOP-BVUKRH3J.localdomain>
2026-06-10 17:43:56 -0400

    Fix CORS: accept array of origins including localhost

--- secondBrain/src/index.ts
+++ secondBrain/src/index.ts
@@ -22,8 +22,19 @@ const app = express();
 
 // Middleware
 app.use(express.json());
+const allowedOrigins = [
+  process.env.FRONTEND_URL,
+  "http://localhost:5173"
+].filter(Boolean) as string[];
+
 app.use(cors({
-  origin: process.env.FRONTEND_URL || "http://localhost:5173",
+  origin: (origin, callback) => {
+    if (!origin || allowedOrigins.includes(origin)) {
+      callback(null, true);
+    } else {
+      callback(new Error(`CORS: origin '${origin}' not allowed`));
+    }
+  },
   credentials: true,
 }));
 
